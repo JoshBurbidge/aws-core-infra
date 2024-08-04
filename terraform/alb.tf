fcd336569ba695650245e3f23c66ce2cae5a8eb9
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-08-04 16:21:03 -0500

    destroy alb

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,28 +1,28 @@
-resource "aws_lb" "main_lb" {
-  name               = "main-lb-2"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = data.aws_security_groups.default_vpc_sgs.ids
-  subnets            = data.aws_subnets.subnets.ids
+# resource "aws_lb" "main_lb" {
+#   name               = "main-lb-2"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = data.aws_security_groups.default_vpc_sgs.ids
+#   subnets            = data.aws_subnets.subnets.ids
 
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "aws_lb_listener" "main_lb_https_listener" {
-  load_balancer_arn = aws_lb.main_lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = data.aws_acm_certificate.cert.arn
+# resource "aws_lb_listener" "main_lb_https_listener" {
+#   load_balancer_arn = aws_lb.main_lb.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   certificate_arn   = data.aws_acm_certificate.cert.arn
 
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      status_code  = 200
-      content_type = "text/plain"
-      message_body = "HTTPS listener default action"
-    }
-  }
+#   default_action {
+#     type = "fixed-response"
+#     fixed_response {
+#       status_code  = 200
+#       content_type = "text/plain"
+#       message_body = "HTTPS listener default action"
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
